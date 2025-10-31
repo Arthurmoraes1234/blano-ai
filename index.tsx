97da6daf4cffbb88940981713e30852f9bc93385
Arthurmoraes1234 <brincadeirasparacriancass@gmail.com>
2025-10-30 22:29:54 -0300

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,21 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+
+// A remoção do "import React from 'react';" e do "<React.StrictMode>"
+// é a melhor forma de forçar o ambiente a usar a injeção de dependências 
+// correta (react-dom/client) e ignorar o erro de 'jsx-runtime'
+// que está preso no ambiente.
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+    // Esta mensagem de erro é importante para o diagnóstico.
+    throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+    // Retiramos o <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+
